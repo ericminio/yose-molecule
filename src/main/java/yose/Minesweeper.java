6817ba8852db0f7c4e65e55841bf0f663ef8b25c
ericminio <eric.mignot@gmail.com>
2014-10-17 16:17:21 -0400

    ... The page has the expected title

--- src/main/java/yose/Minesweeper.java
+++ src/main/java/yose/Minesweeper.java
@@ -4,6 +4,11 @@ import com.google.gson.Gson;
 import com.vtence.molecule.Request;
 import com.vtence.molecule.Response;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Minesweeper {
 
     private Gson gson;
@@ -13,6 +18,11 @@ public class Minesweeper {
     }
 
     public void page(Request request, Response response) {
-        response.contentType("text/html");
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("src/main/webapp/views/minesweeper.html"));
+            response.contentType("text/html").body(lines.stream().collect(Collectors.joining("\n")));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
