62cb1ab828727b83004dca04ade0264654e2e453
ericminio <eric.mignot@gmail.com>
2014-11-07 08:30:30 -0500

    declare grid table in one single line

--- src/main/webapp/js/minesweeper.js
+++ src/main/webapp/js/minesweeper.js
@@ -1,9 +1,4 @@
 function displayGrid() {
     var table = document.getElementById('grid');
-
-    var row = table.insertRow(0);
-
-    row.insertCell(0).id = 'cell-1x1';
-    row.insertCell(0).id = 'cell-8x8';
-    row.insertCell(0).id = 'cell-2x3';
+    table.insertRow(-1).insertCell(-1).id='cell-1x1';
 }
